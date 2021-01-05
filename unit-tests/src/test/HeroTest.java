4f52d30b49d9d92321ee6db83dec1c1d792d8a70
Escias <ljimmy98@hotmail.fr>
2021-01-05 16:47:20 +0100

    hero and enemy unit test complete

--- unit-tests/src/test/HeroTest.java
+++ unit-tests/src/test/HeroTest.java
@@ -18,12 +18,12 @@ public class HeroTest {
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		System.out.println("Avant le démarrage");
+		System.out.println("Avant le démarrage HERO");
 	}
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
-		System.out.println("Après tous les tests");
+		System.out.println("Après tous les tests HERO");
 	}
 
 	@Before
@@ -38,9 +38,9 @@ public class HeroTest {
 	}
 
 	@Test
-	public void testHeroLevelUp() throws Exception {
-		hero.levelUp();
-		assertThat(hero.getLevel(), is(2));
+	public void testHeroProperties() throws Exception {
+		assertThat(hero, hasProperty("name"));
+		assertThat(hero, hasProperty("name", is("Jaina Portvaillant")));
 	}
 
 	@Test
@@ -49,16 +49,31 @@ public class HeroTest {
 	}
 
 	@Test
+	public void testHeroLevel(){
+		assertThat(hero.getLevel(), equalTo(1));
+	}
+
+	@Test
 	public void testHeroAtk(){
+		assertThat(hero.getAtk(), equalTo(2));
+	}
+
+	@Test
+	public void testHeroDmg() throws Exception {
+		hero.takeDamage(3);
+		assertThat(hero.getHp(), allOf(lessThan(20), equalTo(17)));
+	}
+
+	@Test
+	public void testHeroAttack() throws Exception {
 		Enemy enemy = new Enemy("Mob", 1);
 		hero.attack(enemy);
 		assertThat(enemy.getHp(), lessThan(15));
 	}
 
 	@Test
-	public void testHeroProperties() throws Exception {
-		assertThat(hero, hasProperty("name"));
-        assertThat(hero, hasProperty("name", is("Jaina Portvaillant")));
+	public void testHeroLevelUp() throws Exception {
+		hero.levelUp();
+		assertThat(hero.getLevel(), is(2));
 	}
-
 }
