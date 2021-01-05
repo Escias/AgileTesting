1bc11ad5b9eddef529cc934771fd11ddf3f5d0ea
Escias <ljimmy98@hotmail.fr>
2021-01-05 14:10:37 +0100

    sh file

---
+++ unit-tests/compileAndTest.sh
@@ -0,0 +1,11 @@
+cd src &&
+
+javac -d ../bin/ codingfactory/rpgconsole/enemy/*.java &&
+javac -d ../bin/ codingfactory/rpgconsole/hero/*.java &&
+javac -d ../bin/ codingfactory/rpgconsole/game/*.java &&
+
+javac -d ../bin/ test/HeroTest.java &&
+
+cd ../bin &&
+
+java org.junit.runner.JUnitCore test.HeroTest
