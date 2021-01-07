8399001e88f286cb9d0eb45554ef0549c1465546
Theovady Moutty <theomoutty@gmail.com>
2021-01-07 22:41:17 +0100

    add plein de chose

--- acceptance-tests/src/test/acceptance/Configurateur.feature
+++ acceptance-tests/src/test/acceptance/Configurateur.feature
@@ -4,7 +4,31 @@ Feature: Fonctionnalités de la page models
         Then j'appuie sur Commander
         And l'url de la page doit être "https://www.tesla.com/fr_fr/models/design#battery"
 
-  Scenario: Vérification du LOA
+#  Scenario: Vérification du LOA
+#    Given je suis sur la page modelS
+#    Then j'appuie sur Commander
+#    And Le prix du LOA doit être de "768 € /mois"
+#
+#  Scenario: Vérification du Prix de LOA avec pilotage automatique
+#    Given je suis sur la page modelS
+#    Then j'appuie sur Commander
+#    And j'appuie sur pilotage automatique
+#    And j'appuie sur option ajouter et on verifie l'augmentation de "89"
+#
+#  Scenario: Vérification des prix du modelS
+#    Given je suis sur la page modelS
+#    Then j'appuie sur Commander
+#    And j'appuie sur modèle Grande Autonomie Plus
+#    And le prix de LOA du modèle Grande Autonomie doit  être de "768" et "108" d'économie de carburant et un montant du contrat "84990"
+#    And j'appuie sur Performance
+#    And le prix de LOA de la Performance doit  être de "969" et "108" d'économie de carburant et un montant du contrat "101990"
+
+  Scenario: Vérification des listes de localisation
     Given je suis sur la page modelS
     Then j'appuie sur Commander
-    And Le prix du LOA doit être de "768 € /mois"
+    And j'appuie sur Suivant
+    And je choisi les peneus d'hivers
+    And j'appuie sur Suivant x2
+    And j'appuie sur pilotage automatique
+    And j'appuie sur tesla
+    And j'appuie sur localisation et verifie si l'url et le bon
--- acceptance-tests/src/test/acceptance/HomepageSteps.java
+++ acceptance-tests/src/test/acceptance/HomepageSteps.java
@@ -18,6 +18,7 @@ import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.*;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.By;
 
@@ -90,6 +91,25 @@ public class HomepageSteps {
 		}
 	}
 
+	@Then("^j'appuie sur pilotage automatique")
+	public void j_appuie_sur_pilotage_automatique() throws Throwable {
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//span[text()=\"Pilotage automatique\"]")).click();
+		Thread.sleep(1000);
+	}
+
+	@Then("j'appuie sur option ajouter et on verifie l'augmentation de \"([^\"]*)\"$")
+	public void j_appuie_sur_option_ajouter_et_on_verifie_l_augmentation_de(String arg1) throws Throwable {
+		Thread.sleep(1000);
+		int prix1 = Integer.parseInt(driver.findElement(By.cssSelector(".finance-item--price")).getAttribute("innerHTML").substring(12,15));
+		driver.findElement(By.xpath("//span[text()=\"Ajouter cette option\"]")).click();
+		int prix2 = Integer.parseInt(driver.findElement(By.cssSelector(".finance-item--price")).getAttribute("innerHTML").substring(12,15));
+		Thread.sleep(1000);
+		String result = Integer.toString(prix2 - prix1);
+		assertEquals(result, arg1);
+	}
+
+
 	@Then("^l'url de la page doit être \"([^\"]*)\"$")
 	public void l_url_de_la_page_doit_être(String arg1) throws Throwable {
 		for (String windowHandle : driver.getWindowHandles()) {
@@ -98,11 +118,117 @@ public class HomepageSteps {
 		assertEquals(driver.getCurrentUrl(), arg1);
 	}
 
+
+
 	@Then("^Le prix du LOA doit être de \"([^\"]*)\"$")
 	public void le_prix_du_LOA_doit_être_de(String arg1) throws Throwable {
-		assertEquals(driver.findElement(By.xpath("//p[@class='finance-item--price finance-item--price-before-savings']")), arg1);
-		Boolean lien1 = driver.findElements(By.xpath("//p[text() = \"" + arg1 + "\"]")).size() > 0;
-		assertThat(lien1, is(true));
+		String loa = driver.findElement(By.cssSelector(".finance-item--price")).getAttribute("innerHTML");
+		boolean check = loa.contains(arg1);
+		assertThat(check, is(true));
+	}
+
+	@Then("^j'appuie sur modèle Grande Autonomie Plus$")
+	public void j_appuie_sur_modèle_Grande_Autonomie_Plus() throws Throwable {
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//span[text()=\"Grande Autonomie Plus\"]")).click();
+		Thread.sleep(1000);
+	}
+
+	@Then("^le prix de LOA du modèle Grande Autonomie doit  être de \"([^\"]*)\" et \"([^\"]*)\" d'économie de carburant et un montant du contrat \"([^\"]*)\"$")
+	public void le_prix_de_LOA_du_modèle_Grande_Autonomie_doit_être_de_et_d_économie_de_carburant_et_un_montant_du_contrat(String arg1, String arg2, String arg3) throws Throwable {
+		Thread.sleep(1000);
+		int prix1 = Integer.parseInt(driver.findElement(By.cssSelector(".finance-item--price.finance-item--price-before-savings")).getAttribute("innerHTML").replaceAll("[^\\d.]", ""));
+		String result1 = Integer.toString(prix1);
+		assertEquals(result1, arg1);
+
+		int prix2 = Integer.parseInt(driver.findElement(By.xpath("//p[contains(text(), \"" + arg2 + "\")]")).getAttribute("innerHTML").replaceAll("[^\\d.]", ""));
+		String result2 = Integer.toString(prix2);
+		assertEquals(result2, arg2);
+		Thread.sleep(1000);
+
+		driver.findElement(By.xpath("//span[@class=\"tds-arrow tds-icon tds-icon--alt tds-icon-chevron--down tds-icon--xsmall tds-o-icon-chevron--down\"]")).click();
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//li[contains(text(), \"Comptant\")]")).click();
+		Thread.sleep(5000);
+		int prix3 =  Integer.parseInt(driver.findElement(By.cssSelector(".finance-item--price.finance-item--price-before-savings")).getAttribute("innerHTML").replaceAll("[^\\d.]", ""));
+		String result3 = Integer.toString(prix3);
+		assertEquals(result3, arg3);
+	}
+
+	@Then("^j'appuie sur Performance$")
+	public void j_appuie_sur_Performance() throws Throwable {
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//span[text()=\"Performance\"]")).click();
+		driver.findElement(By.xpath("//span[@class=\"tds-arrow tds-icon tds-icon--alt tds-icon-chevron--down tds-icon--xsmall tds-o-icon-chevron--down\"]")).click();
+		driver.findElement(By.xpath("//li[contains(text(), \"LOA\")]")).click();
+		Thread.sleep(1000);
+	}
+
+	@Then("^le prix de LOA de la Performance doit  être de \"([^\"]*)\" et \"([^\"]*)\" d'économie de carburant et un montant du contrat \"([^\"]*)\"$")
+	public void le_prix_de_LOA_de_la_Performance_doit_être_de_et_d_économie_de_carburant_et_un_montant_du_contrat(String arg1, String arg2, String arg3) throws Throwable {
+		Thread.sleep(1000);
+		int prix1 = Integer.parseInt(driver.findElement(By.cssSelector(".finance-item--price.finance-item--price-before-savings")).getAttribute("innerHTML").replaceAll("[^\\d.]", ""));
+		String result1 = Integer.toString(prix1);
+		assertEquals(result1, arg1);
+
+		int prix2 = Integer.parseInt(driver.findElement(By.xpath("//p[contains(text(), \"" + arg2 + "\")]")).getAttribute("innerHTML").replaceAll("[^\\d.]", ""));
+		String result2 = Integer.toString(prix2);
+		assertEquals(result2, arg2);
+		Thread.sleep(1000);
+
+		driver.findElement(By.xpath("//span[@class=\"tds-arrow tds-icon tds-icon--alt tds-icon-chevron--down tds-icon--xsmall tds-o-icon-chevron--down\"]")).click();
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//li[contains(text(), \"Comptant\")]")).click();
+		Thread.sleep(5000);
+		int prix3 =  Integer.parseInt(driver.findElement(By.cssSelector(".finance-item--price.finance-item--price-before-savings")).getAttribute("innerHTML").replaceAll("[^\\d.]", ""));
+		String result3 = Integer.toString(prix3);
+		assertEquals(result3, arg3);
+	}
+
+	@Then("^j'appuie sur Suivant$")
+	public void j_appuie_sur_Suivant() throws Throwable {
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//button[text()=\"Suivant\"]")).click();
+		Thread.sleep(1000);
+	}
+
+	@Then("^je choisi les peneus d'hivers$")
+	public void je_choisi_les_peneus_d_hivers() throws Throwable {
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//span[text()=\"Ajouter des pneus d'hiver\"]")).click();
+		Thread.sleep(1000);
+	}
+
+	@Then("^j'appuie sur Suivant x(\\d+)$")
+	public void j_appuie_sur_Suivant_x(int arg1) throws Throwable {
+		Thread.sleep(1000);
+		for (int i = 0; i < arg1; i++) {
+			driver.findElement(By.xpath("//button[text()=\"Suivant\"]")).click();
+			Thread.sleep(5000);
+		}
+	}
+
+	@Then("^j'appuie sur tesla$")
+	public void j_appuie_sur_tesla() throws Throwable {
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//a[@class=\"tsla-header-main--logo tds-icon tds-icon-wordmark\"]")).click();
+		Thread.sleep(1000);
+	}
+
+	@Then("^j'appuie sur localisation et verifie si l'url et le bon$")
+	public void j_appuie_sur_localisation_et_verifie_si_l_url_et_le_bon() throws Throwable {
+
+		driver.findElement(By.xpath("//a[text()=\"France\"]")).click();
+		for(int i = 0; i < 6; i++){
+			driver.findElement(By.id("page-new-homepage")).click();
+			WebElement test = driver.findElement(By.id("page-new-homepage"));
+			test.sendKeys(Keys.DOWN);
+			Thread.sleep(2500);
+		}
+		driver.findElement(By.xpath("//a[text()=\"Localisations\"]")).click();
+		Thread.sleep(2500);
+		assertThat(driver.getCurrentUrl(), containsString("https://www.tesla.com/fr_FR/findus/list"));
+
 	}
 
 
