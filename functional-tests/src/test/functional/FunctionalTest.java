8f794287eacac8a5cd5cbc3bd970149c05ba1597
Escias <ljimmy98@hotmail.fr>
2021-01-08 10:52:37 +0100

    functionnal correct

--- functional-tests/src/test/functional/FunctionalTest.java
+++ functional-tests/src/test/functional/FunctionalTest.java
@@ -17,29 +17,26 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.By;
 
-import org.openqa.selenium.By;
-
 public class FunctionalTest {
 
-	private WebDriver driver;
+    private WebDriver driver;
 
     @Before
     public void setUp() throws Exception {
         System.setProperty("webdriver.chrome.driver","/Library/Java/JUNIT/chromedriver");
-		driver = new ChromeDriver();
+        driver = new ChromeDriver();
+        // Seems no more working in last Chrome versions
+        // driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         driver.get("https://www.tesla.com/fr_FR/");
-	    	// Seems no more working in last Chrome versions
-		// driver.manage().window().maximize();
-  		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-        driver.get("https://www.tesla.com/fr_FR/");
-     }
+    }
 
     // Test de la Story #1-homepage (https://trello.com/c/WKTneu9o/1-homepage)
-	@Test
+    @Test
     public void testHomepage() throws Exception {
-		assertEquals(driver.getTitle(), "Voitures électriques, énergie solaire et propre | Tesla France");
-		// TODO
-		// To Be Completed By Coders From Coding Factory
+        assertEquals(driver.getTitle(), "Voitures électriques, énergie solaire et propre | Tesla France");
+        // TODO
+        // To Be Completed By Coders From Coding Factory
     }
 
     @Test
@@ -56,43 +53,13 @@ public class FunctionalTest {
     }
 
     @Test
-    public void testPunchlines() throws Exception {
-        Boolean title1 = driver.findElements(By.xpath("//h1[contains(text(), 'Model 3')]")).size() > 0;
-        assertThat(title1, is(true));
-        Boolean title2 = driver.findElements(By.xpath("//h1[contains(text(), 'Model S')]")).size() > 0;
-        assertThat(title2, is(true));
-        Boolean title3 = driver.findElements(By.xpath("//h1[contains(text(), 'Model X')]")).size() > 0;
-        assertThat(title3, is(true));
-        Boolean title4 = driver.findElements(By.xpath("//h1[contains(text(), 'Model Y')]")).size() > 0;
-        assertThat(title4, is(true));
-        Boolean title5 = driver.findElements(By.xpath("//h1[contains(text(), \"Systèmes d"+"'"+"énergie solaire et Powerwalls\")]")).size() > 0;
-        assertThat(title5, is(true));
-    }
-
-    @Test
     public void testMenu() throws Exception {
-        String lien1 = driver.findElement(By.xpath("//a[text() = 'Model S']")).getAttribute("href");
-        assertThat(lien1, is("https://www.tesla.com/fr_fr/models"));
-        String lien2 = driver.findElement(By.xpath("//a[text() = 'Model 3']")).getAttribute("href");
-        assertThat(lien2, is("https://www.tesla.com/fr_fr/model3"));
-        String lien3 = driver.findElement(By.xpath("//a[text() = 'Model X']")).getAttribute("href");
-        assertThat(lien3, is("https://www.tesla.com/fr_fr/modelx"));
-        String lien4 = driver.findElement(By.xpath("//a[text() = 'Cybertruck']")).getAttribute("href");
-        assertThat(lien4, is("https://www.tesla.com/fr_fr/cybertruck"));
-        String lien5 = driver.findElement(By.xpath("//a[text() = 'Roadster']")).getAttribute("href");
-        assertThat(lien5, is("https://www.tesla.com/fr_fr/roadster"));
-        String lien6 = driver.findElement(By.xpath("//a[text() = 'Recharger']")).getAttribute("href");
-        assertThat(lien6, is("https://www.tesla.com/fr_fr/charging"));
+        List<String> listMenu = Arrays.asList("/fr_fr/models", "/fr_fr/model3", "/fr_fr/modelx", "/fr_fr/modely", "/fr_fr/powerwall", "/fr_fr/charging");
+        for(String menu : listMenu) {
+            String url = "https://www.tesla.com"+menu;
+            assertEquals(driver.findElement(By.xpath("//a[contains(@href, \"" + menu + "\")]")).getAttribute("href"), url);
+        }
     }
-
-    @Test
-    public void testBurger() throws Exception {
-        //nav role="navigation" id="block-hamburgerdesktop"
-    }
-
-    // Test de la Story n ...
-    // TODO
-    // To Be Completed By Coders From Coding Factory
 
     @After
     public void tearDown() throws Exception {
